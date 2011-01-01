07aa6fa741a55966a43ca8a32213feeaa2daf798
Sheheryar Sewani <sheheryarsewani@gmail.com>
2011-01-01 15:50:25 -0500

    The response body to get document now includes the previous revision numbers.  Added Request builder to help create the more complicated http requests.  Can create documents through the interface, update still not tested and the code requires quiet a bit of clean up

--- cushion/client.py
+++ cushion/client.py
@@ -1,35 +1,94 @@
 import httplib2
+import simplejson as json
+from urllib import urlencode
+from functools import partial
+
+
+class RequestBuilder(object):
+    """
+        RequestBuilder is responsible for building
+        the http request based on the http method type
+        and URL that is being accessed
+    """
+    def __init__(
+        self,
+        username,
+        password,
+        base_uri
+    ):
+        self.username = username
+        self.password = password
+        self.http_client = httplib2.Http()
+        self.http_client.base_uri = base_uri
+
+    def build(
+        self,
+        method_part,
+        uri_part,
+        options
+    ):
+        """
+        Build the request, returns callable which in turn
+        returns the http response and the http content body
+        """
+        body = None
+        uri =  "/".join(
+            [
+                self.http_client.base_uri,
+                uri_part,
+                options['id']
+            ]
+        )
+        if method_part.upper() == "GET":
+            uri = uri + '?%s' % urlencode({
+                'revs': 'true'
+            })
+        elif method_part.upper() == "PUT":
+            del options['id']
+            body = options
+
+        return partial(
+            self.http_client.request,
+            uri,
+            headers={'Content-Type': 'application/json'},
+            method=method_part.upper(),
+            body=json.dumps(body)
+        )
 
 
 class Part(object):
     def __init__(
         self,
         parts,
-        client
+        request_builder
     ):
         self.parts = parts
-        self.client = client
-        
-    def __call__(self, **kwargs):
-        uri = '/'.join(self.parts)
-        return self._request(uri, kwargs)
+        self.request_builder = request_builder
 
-    def _request(self, uri, kwargs):
-        (method, uri_part) = uri.split("/")
-        url =  "/".join([self.client.base_uri, uri_part, kwargs['id']])
-        resp, content = self.client.request(
-            url,
-            method.upper()
+    def __call__(self, **kwargs):
+        return self._request(self.parts, kwargs)
+
+    def _request(self, method_parts, kwargs):
+        (method_part, uri_part) = method_parts[0], method_parts[1]
+        request = self.request_builder.build(
+            uri_part,
+            method_part,
+            kwargs
         )
+        response, content = request()
         return content
 
     def __getattr__(self, name):
         return Part(
             self.parts + [name],
-            self.client
+            self.request_builder
         )
 
+
 class Cushion(object):
+    """
+        Cushion is the primary interface to CouchDB
+    """
     def __init__(
         self,
         base_uri,
@@ -40,13 +99,15 @@ class Cushion(object):
         self.username = username
         self.password = password
 
-    def _get_http_client(self):
-        h = httplib2.Http()
-        h.base_uri = self.base_uri
-        return h
+    def _get_request_builder(self):
+        return RequestBuilder(
+            self.username,
+            self.password,
+            self.base_uri
+        )
 
     def __getattr__(self, name):
         return Part(
             [name],
-            self._get_http_client()
+            self._get_request_builder()
         )
