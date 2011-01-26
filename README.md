699aa8320865fb31ae1203b6a1d39042db05ef26
Sheheryar Sewani <sheheryarsewani@gmail.com>
2011-01-26 01:24:08 -0500

    first release. updated documentation, uploaded to pypi

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ This is a Python wrapper for the [CouchDB Document API](http://wiki.apache.org/c
 
 For example,
 
+	from cushion import api
+	
     c = api.Cushion("http://localhost:5984/albums",
 		username='abc',
 		password='xyz'
@@ -12,7 +14,12 @@ For example,
 
 returns the following JSON object:
 
-    {'Band': 'The Postal Service', '_rev': '1-f319aeef4dbb8c232f0257cf2a9ae64b', '_id': '1b0c7bb19b1bbacf7f567bf379000d9c', 'Name': 'Give Up'}
+    {
+		'Band': 'The Postal Service',
+		'_rev':'1-f319aeef4dbb8c232f0257cf2a9ae64b',
+		'_id': '1b0c7bb19b1bbacf7f567bf379000d9c',
+		'Name': 'Give Up'
+	}
 
 alternatively, the database can be specified at runtime,
 
@@ -25,11 +32,35 @@ alternatively, the database can be specified at runtime,
 
 which also returns:
 
-	{'Band': 'The Postal Service', '_rev': '1-f319aeef4dbb8c232f0257cf2a9ae64b', '_id': '1b0c7bb19b1bbacf7f567bf379000d9c', 'Name': 'Give Up'}
+	{
+		'Band': 'The Postal Service',
+		'_rev':'1-f319aeef4dbb8c232f0257cf2a9ae64b',
+		'_id': '1b0c7bb19b1bbacf7f567bf379000d9c',
+		'Name': 'Give Up'
+	}
+
+## Examples
+
+To retrieve a document including previous version numbers:
+
+	c.get.albums(id='1b0c7bb19b1bbacf7f567bf379000d9c', revs='true')
+
+to get a listing of all documents in a database:
+
+	c.get.albums._all_docs(startkey="1", limit=100)
+
+to create a document with a document id:
+
+	c.put.albums(id='c3ea09f0-9758-480c-aa25-0f71a9caa446', Name='Transatlanticism', Band='Death Cab For Cutie')
+
+alternatively, to create a document without supplying the document id:
+
+	c.post.albums(Name='In Ghost Colors', Band='Cut Copy')
+
 
 ## Installation
 
-    easy_install cushion (soon)
+    easy_install cushion
 
 ## Links
 
--- setup.py
+++ setup.py
@@ -7,11 +7,63 @@ except ImportError:
 
 setup(
     name='cushion',
-    version='0.1',
+    version='0.2',
     author='Sheheryar Sewani',
     author_email='sheheryar.sewani@gmail.com',
     url='http://github.com/sheysrebellion/cushion',
     description='A simple wrapper for CouchDB',
+    long_description="""
+        Cushion: a simple CouchDB Document API wrapper
+
+        This is a Python wrapper for the CouchDB Document API (http://wiki.apache.org/couchdb/HTTP_Document_API).
+        It was created primarily as an exercise to learn CouchDB's document API.
+        The library attempts to expose a very simple interface that closely resembles REST.
+
+        For example,
+
+        	from cushion import api
+
+            c = api.Cushion("http://localhost:5984/albums",
+        		username='abc',
+        		password='xyz'
+        	)
+        	c.get(id='1b0c7bb19b1bbacf7f567bf379000d9c')
+
+        returns the following JSON object:
+
+            {
+        		'Band': 'The Postal Service',
+        		'_rev':'1-f319aeef4dbb8c232f0257cf2a9ae64b',
+        		'_id': '1b0c7bb19b1bbacf7f567bf379000d9c',
+        		'Name': 'Give Up'
+        	}
+
+        alternatively, the database can be specified at runtime,
+
+        	c = api.Cushion("http://localhost:5984",
+        		username='abc',
+        		password='xyz',
+        		timeout=5
+        	)
+        	c.get.albums(id='1b0c7bb19b1bbacf7f567bf379000d9c')
+
+        which also returns:
+
+        	{
+        		'Band': 'The Postal Service',
+        		'_rev':'1-f319aeef4dbb8c232f0257cf2a9ae64b',
+        		'_id': '1b0c7bb19b1bbacf7f567bf379000d9c',
+        		'Name': 'Give Up'
+        	}
+
+        to create a document with a document id:
+
+            c.put.albums(id='c3ea09f0-9758-480c-aa25-0f71a9caa446', Name='Transatlanticism', Band='Death Cab For Cutie')
+
+        alternatively, to create a document without supplying the document id:
+
+            c.post.albums(Name='In Ghost Colors', Band='Cut Copy')
+    """,
 
     classifiers=[
         'Development Status :: 3 - Alpha',
